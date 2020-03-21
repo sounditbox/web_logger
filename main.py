9b635c7b6ccc58b29eea4615047df094f42397ec
Ruslan Mutygullin <sounditbox@gmail.com>
2020-03-21 13:06:00 +0500

    listen to environment var

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
+import os
 
-
+PORT = os.environ.get('PORT')
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', ''])
 def bla():
-    if request.method
     return 'blablabla'
 
 if __name__ == '__main__':
-    app.run(host='', port=8000)
+    app.run(host='', port=PORT)
